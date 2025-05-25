51ac5da9490fdaa6e60727a5887a7ba3425d3e08
spawlov <107248409+spawlov@users.noreply.github.com>
2025-05-26 02:53:07 +0700

    Removed create tables after start app

--- src/main.py
+++ src/main.py
@@ -2,22 +2,15 @@ from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
 import uvicorn
-
-from api import router as api_router
 from fastapi import FastAPI
 
-from core.models import Base
-from core.models.db_helper import db_helper
+from api import router as api_router
 from core.settings import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
 
 
 main_app = FastAPI(
