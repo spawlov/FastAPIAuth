4f41be60efd9cd39bb798271935ea875ca654ed9
spawlov <107248409+spawlov@users.noreply.github.com>
2025-06-08 15:31:43 +0700

    Added test for encode and decode jwt

---
+++ src/tests/unit/api/api_v1/auth/conftest.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+import pytest
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+
+@pytest.fixture
+async def session():
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:")
+    async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    async with async_session() as session:
+        yield session
+
+
+@pytest.fixture
+def rsa_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    return {
+        "private_key": private_pem.decode("utf-8"),
+        "public_key": public_pem.decode("utf-8"),
+    }
+
+
+@pytest.fixture
+def valid_access_token_payload() -> dict[str, Any]:
+    jwt_payload = {
+        "type": "access",
+        "sub": "1",
+        "username": "test",
+    }
+    return jwt_payload
---
+++ src/tests/unit/api/api_v1/auth/test_encode_decode_jwt.py
@@ -0,0 +1,54 @@
+import uuid
+from datetime import timedelta
+from typing import Any
+
+import pytest
+from jwt import InvalidKeyError, InvalidTokenError
+
+from api.api_v1.auth.utils import decode_jwt, encode_jwt
+from tests.unit.api.api_v1.auth.conftest import rsa_keys
+
+
+def test_encode_decode_jwt(
+    valid_access_token_payload: dict[str, Any],
+    rsa_keys: dict[str, str],  # noqa: F811
+) -> None:
+    jti = str(uuid.uuid4())
+    access_token = encode_jwt(
+        payload=valid_access_token_payload,
+        expires_in=timedelta(minutes=5),
+        jti=jti,
+        private_key=rsa_keys["private_key"],
+    )
+    result = decode_jwt(
+        access_token,
+        public_key=rsa_keys["public_key"],
+    )
+    assert result["sub"] == valid_access_token_payload["sub"]
+    assert result["username"] == valid_access_token_payload["username"]
+    assert result["type"] == valid_access_token_payload["type"]
+    assert result["jti"] == jti
+    assert "iat" in result
+    assert "exp" in result
+
+
+def test_failure_encode_jwt(
+    valid_access_token_payload: dict[str, Any],
+) -> None:
+    with pytest.raises(InvalidKeyError):
+        encode_jwt(
+            payload=valid_access_token_payload,
+            expires_in=timedelta(minutes=5),
+            jti=str(uuid.uuid4()),
+            private_key="wrong_private_key",
+        )
+
+
+def test_failure_decode_jwt(
+    rsa_keys: dict[str, str],  # noqa: F811
+) -> None:
+    with pytest.raises(InvalidTokenError):
+        decode_jwt(  # noqa: S106
+            token="Invalid_JWT",
+            public_key=rsa_keys["public_key"],
+        )
