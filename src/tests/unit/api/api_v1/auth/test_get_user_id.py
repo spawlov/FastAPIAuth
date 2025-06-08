1bb9e6c87b394dd23fd3077152de5d9cd22c6823
spawlov <107248409+spawlov@users.noreply.github.com>
2025-06-09 01:25:27 +0700

    Added tests for test_get_user_id

--- src/tests/unit/api/api_v1/auth/test_get_user_id.py
+++ src/tests/unit/api/api_v1/auth/test_get_user_id.py
@@ -48,3 +48,33 @@ class TestGetUserId:
             await get_user_id(session, valid_refresh_token_payload, "access")
         assert exc.value.status_code == status.HTTP_401_UNAUTHORIZED
         assert exc.value.detail == "Invalid token type: 'refresh', expected 'access'"
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("payload", [None, "WrongType"])
+    async def test_get_user_with_invalid_token_type(
+        self,
+        mocker,
+        session: AsyncSession,
+        payload: str | None,
+    ) -> None:
+        mocker.patch("api.api_v1.auth.utils.is_token_revoked", return_value=False)
+
+        with pytest.raises(HTTPException) as exc:
+            await get_user_id(session, payload, "access")
+        assert exc.value.status_code == status.HTTP_400_BAD_REQUEST
+        assert exc.value.detail == "No token_payload provided or wrong token_payload type"
+
+    @pytest.mark.asyncio
+    async def test_get_user_without_sub(
+        self,
+        mocker,
+        session: AsyncSession,
+        valid_access_token_payload: dict[str, Any],
+    ) -> None:
+        mocker.patch("api.api_v1.auth.utils.is_token_revoked", return_value=False)
+        del valid_access_token_payload["sub"]
+
+        with pytest.raises(HTTPException) as exc:
+            await get_user_id(session, valid_access_token_payload, "access")
+        assert exc.value.status_code == status.HTTP_401_UNAUTHORIZED
+        assert exc.value.detail == "Invalid token"
