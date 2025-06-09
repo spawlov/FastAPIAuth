465e4f7628408f8dccaefc0197ba53201604fe11
spawlov <107248409+spawlov@users.noreply.github.com>
2025-06-09 12:27:16 +0700

    Added clearing RATE_LIMIT_DATA before test

--- src/tests/integration/api/api_v1/auth/test_login.py
+++ src/tests/integration/api/api_v1/auth/test_login.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import HTTPException, status
 from httpx import AsyncClient
 
+from api.api_v1.auth.utils import RATE_LIMIT_DATA
 from core.models import User
 from core.schemas.auth import TokenInfo
 
@@ -16,6 +17,7 @@ class TestLogin:
         access_token: str,
         refresh_token: str,
     ):
+        RATE_LIMIT_DATA.clear()
         mocker.patch("api.api_v1.auth.auth.get_auth_user", return_value=mock_user)
         mocker.patch("api.api_v1.auth.auth.get_access_token", return_value=access_token)
         mocker.patch("api.api_v1.auth.auth.get_refresh_token", return_value=refresh_token)
@@ -36,11 +38,12 @@ class TestLogin:
         assert response.token_type == "Bearer"  # noqa: S105
 
     @pytest.mark.asyncio
-    async def test_invalid_credentials(
+    async def test_login_with_invalid_credentials(
         self,
         mocker,
         async_client: AsyncClient,
     ):
+        RATE_LIMIT_DATA.clear()
         mocker.patch(
             "api.api_v1.auth.auth.get_auth_user",
             side_effect=HTTPException(
@@ -61,7 +64,7 @@ class TestLogin:
         assert result.json()["detail"] == "Invalid username or password"
 
     @pytest.mark.asyncio
-    async def test_rate_limited(
+    async def test_login_with_rate_limited(
         self,
         mocker,
         async_client: AsyncClient,
@@ -69,6 +72,7 @@ class TestLogin:
         access_token: str,
         refresh_token: str,
     ):
+        RATE_LIMIT_DATA.clear()
         mocker.patch("api.api_v1.auth.auth.get_auth_user", return_value=mock_user)
         mocker.patch("api.api_v1.auth.auth.get_access_token", return_value=access_token)
         mocker.patch("api.api_v1.auth.auth.get_refresh_token", return_value=refresh_token)
@@ -77,7 +81,7 @@ class TestLogin:
             "username": "test_user",
             "password": "user_pass",
         }
-        for _ in range(5 - 2):
+        for _ in range(5):
             result = await async_client.post(
                 url="/api/v1/auth/login",
                 data=form_data,
